ec173a8abd6f042eb97fcfccd68ef179d93c3385
Daffa Fauzan <dafazanlbs@gmail.com>
2024-02-22 13:53:20 +0700

    New card design

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -6,33 +6,37 @@ import { useRouter } from "next/navigation";
 export default function Content() {
   const { push } = useRouter();
   return (
-    <>
+    <div className='h-screen '>
 
-      <h1 className="text-center md:mb-20 sm:mb-16 mb-5 md:mt-20 sm:mt-16 mt-5 text-3xl font-bold text-sky-500">Content</h1>
+      <h1 className="text-center md:mb-20 sm:mb-16 mb-5 md:mt-24 sm:mt-16 mt-5 text-3xl font-bold text-sky-500">Content</h1>
+      <div className='w-full h-full px-36 pb-24'>
 
-      <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 justify-items-center mb-20">
-        <a href="/admin/blog">
-          <div className="w-80 h-40 border-4 rounded-lg flex justify-center bg-slate-50">
-            <h1 className="m-auto text-center my-auto text-2xl font-bold">BLOG</h1>
-          </div>
-        </a>
-        <a href="/admin/brand">
-          <div className="w-80 h-40 border-4 rounded-lg flex justify-center bg-slate-50">
-            <h1 className="m-auto text-center my-auto text-2xl font-bold">BRAND</h1>
-          </div>
-        </a>
-        <a href="/admin/portofolio">
-          <div className="w-80 h-40 border-4 rounded-lg flex justify-center bg-slate-50">
-            <h1 className="m-auto text-center my-auto text-2xl font-bold">PORTOFOLIO</h1>
-          </div>
-        </a>
+        <div className="flex md:flex-row flex-col justify-between gap-5 sm:grid-cols-2 justify-items-center mb-28">
+          <a href="/admin/blog">
+            <div className="w-80 h-40  rounded-lg flex justify-center bg-blue-300">
+              <h1 className="m-auto text-center text-blue-800 my-auto text-2xl font-bold">BLOG</h1>
+            </div>
+          </a>
+          <a href="/admin/brand">
+            <div className="w-80 h-40  rounded-lg flex justify-center bg-blue-300">
+              <h1 className="m-auto text-center text-blue-800 my-auto text-2xl font-bold">BRAND</h1>
+            </div>
+          </a>
+          <a href="/admin/portofolio">
+            <div className="w-80 h-40  rounded-lg flex justify-center bg-blue-300">
+              <h1 className="m-auto text-center text-blue-800 my-auto text-2xl font-bold">PORTOFOLIO</h1>
+            </div>
+          </a>
 
-        <button onClick={() => {
-          signOut(auth);
+        </div>
+        <div>
+          <button className='bg-red-500 px-3 py-2 rounded-md text-white text-xl font-semibold' onClick={() => {
+            signOut(auth);
 
-          push("/");
-        }}>logout</button>
+            push("/");
+          }}>SIGN OUT</button>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
